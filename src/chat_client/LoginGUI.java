bf3ff772089217ea1f00474cdb5d6232e056c4c7
Mårten Persson <Morre@Martens-MacBook-Pro.local>
2015-03-03 15:41:48 +0100

    Add loginGUI

---
+++ src/chat_client/LoginGUI.java
@@ -0,0 +1,71 @@
+package chat_client;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class LoginGUI extends JPanel{
+
+	private String userName;
+	private JTextField tfUserName;
+	private JButton btnEnter;
+	private JLabel lblText;
+	private Font font;
+	
+	public LoginGUI() {
+		JFrame frame = new JFrame("UserName");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(loginPanel());
+		frame.pack();	
+		frame.setVisible(true);
+	}
+	
+	public JPanel loginPanel(){
+		JPanel panel = new JPanel();
+		font = new Font("Monospaced", Font.BOLD, 22);
+		
+		panel.setPreferredSize(new Dimension(400,200));
+		panel.setLayout(new GridLayout(3,1,5,5));
+		
+		lblText = new JLabel("Select username!", JLabel.CENTER);
+		lblText.setFont(font);
+		tfUserName = new JTextField("type here..");
+		btnEnter = new JButton("Enter username");
+		
+		panel.add(lblText);
+		panel.add(tfUserName);
+		panel.add(btnEnter);
+	
+		btnEnter.addActionListener(new ButtonListener());
+		
+		return panel;
+	}
+	
+	public String getUserName(){
+		userName = tfUserName.getText();
+		return userName;
+	}
+	
+	private class ButtonListener implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			if(btnEnter.isSelected()){
+				
+			}
+		}
+		
+	}
+	public static void main(String[] args) {
+		LoginGUI gu = new LoginGUI();
+		
+	}
+	
+}
