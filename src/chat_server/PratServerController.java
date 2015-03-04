7db262bb3d996c50cfc1f643a020d8b26fe77054
JonasJR <jonas.remgard@gmail.com>
2015-03-04 10:15:30 +0100

    Server improvements. Added remove and find funktion

--- src/chat_server/PratServerController.java
+++ src/chat_server/PratServerController.java
@@ -6,32 +6,56 @@ public class PratServerController {
 	private ArrayList<Client> clients;
 	private Client tempclient;
 
-	public PratServerController(){
+	public PratServerController() {
 		tempclient = null;
 		clients = new ArrayList<Client>();
 	}
-	public void addClient(Connection connection, String id){
+
+	public void addClient(Connection connection, String id) {
 		tempclient = new Client();
 		tempclient.setConnection(connection);
 		tempclient.setId(id);
 		clients.add(tempclient);
 		System.out.println("Client added to client-list");
 	}
-	public void removeClient(Client c){
-		
-	}
-	public Client findUser(String id){
-		return null;
-	}
-	public void sendMessage(Message m, ArrayList<Client> clients){
-		
-	}
-	public void extractRecipients(Message m){
-		if(m.all==true){
-			sendMessage(m, clients);
-		}else{
-			
+
+	public void removeClient(String id) {
+		boolean removed = false;
+		for (Client client : clients) {
+			if (client.getId() == id) {
+				clients.remove(client);
+				removed = true;
+			} else {
+			}
+		}
+		if (removed) {
+			System.out.println("Client " + id + " removed from connections");
+		} else {
+			System.out.println("No client with the name " + id + " was found");
 		}
 	}
-	
+
+	public Client findUser(String id) {
+		for (Client client : clients) {
+			if (client.getId() == id) {
+				return client;
+			}
+		}
+		System.out.println("No client with the name " + id + " was found");
+		return null;
+
+	}
+
+	public void sendMessage(Message m, ArrayList<Client> clients) {
+
+	}
+
+	public void extractRecipients(Message m) {
+		if (m.all == true) {
+			sendMessage(m, clients);
+		} else {
+
+		}
+	}
+
 }
