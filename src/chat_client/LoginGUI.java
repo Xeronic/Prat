e03f890a27b04f09f750209d56ac5189bb3bb3e9
Marten <persson_dunk@msn.com>
2015-03-04 14:40:36 +0100

    GUi update

--- src/chat_client/LoginGUI.java
+++ src/chat_client/LoginGUI.java
@@ -1,68 +1,88 @@
 package chat_client;
 
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
 
 public class LoginGUI {
 
-	private String userName;
-	private JTextField tfUserName;
+	private String userName, ipAddress;
+	private JTextField tfUserName, tfIpAddress;
 	private JButton btnEnter;
 	private JLabel lblText;
-	private Font font;
 	private ClientController controller;
 	private JFrame frame;
-	private Font lblFont, btnFont, txtFont;
+	private Font btnFont, borderFont;
+	private JLabel inputIpAddress, inputUserName;
 	
 	public LoginGUI(ClientController controller) {
 		this.controller = controller;
-		frame = new JFrame("UserName");
+		frame = new JFrame("Login window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(loginPanel());
 		frame.pack();	
 		frame.setVisible(true);
+		frame.setLocation(400, 200);
 	}
 	
 	public JPanel loginPanel(){
 		JPanel panel = new JPanel();
-		lblFont = new Font("Monospaced", Font.BOLD, 22);
 		btnFont = new Font("Sanserif", Font.ROMAN_BASELINE, 22);
-		txtFont = new Font("Sanserif", Font.BOLD, 16);
-		
+		borderFont = new Font("Sanserif", Font.ROMAN_BASELINE, 28);
 		panel.setPreferredSize(new Dimension(400,200));
-		panel.setLayout(new GridLayout(3,1,5,5));
+		panel.setLayout(new BorderLayout());
 		panel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-		
-		lblText = new JLabel("Select username!", JLabel.CENTER);
-		lblText.setFont(lblFont);
-		tfUserName = new JTextField();
-		tfUserName.setFont(txtFont);
-		btnEnter = new JButton("Enter username");
+
+		btnEnter = new JButton("Enter");
 		btnEnter.setFont(btnFont);
-		
-		panel.add(lblText);
-		panel.add(tfUserName);
-		panel.add(btnEnter);
+	
+		panel.add(btnEnter, BorderLayout.SOUTH);
+		panel.add(centrePanel(), BorderLayout.CENTER);
+		panel.setBackground(Color.ORANGE);
 	
 		btnEnter.addActionListener(new ButtonListener());
 		
 		return panel;
 	}
 	
+	public JPanel centrePanel(){
+		JPanel centrePanel = new JPanel();
+		JPanel userNamePanel = new JPanel(new BorderLayout());
+		JPanel ipAddressPanel = new JPanel(new BorderLayout());
+		centrePanel.setLayout(new GridLayout(2,1));
+		centrePanel.setBackground(Color.ORANGE);
+		inputIpAddress = new JLabel("Ip address:");
+		inputIpAddress.setBackground(Color.ORANGE);
+		inputIpAddress.setOpaque(true);
+		inputUserName = new JLabel("UserName:");
+		inputUserName.setBackground(Color.ORANGE);
+		inputUserName.setOpaque(true);
+		tfUserName = new JTextField();
+		tfUserName.setSize(new Dimension(1009,2000));
+		tfIpAddress = new JTextField();
+		centrePanel.setBorder(BorderFactory.createTitledBorder(null, "Login", TitledBorder.DEFAULT_JUSTIFICATION, 
+				TitledBorder.DEFAULT_POSITION, borderFont, Color.BLACK));
+		userNamePanel.add(inputUserName,BorderLayout.WEST);
+		userNamePanel.add(tfUserName, BorderLayout.CENTER);
+		ipAddressPanel.add(inputIpAddress, BorderLayout.WEST);
+		ipAddressPanel.add(tfIpAddress, BorderLayout.CENTER);
+		centrePanel.add(userNamePanel);
+		centrePanel.add(ipAddressPanel);
+		
+		return centrePanel;
+	}
+	
 	public String getUserName(){
 		userName = tfUserName.getText();
 		return userName;
+	}
+	
+	public String getIpAddress(){
+		ipAddress = tfIpAddress.getText();
+		return ipAddress;
 	}
 	
 	private class ButtonListener implements ActionListener {
